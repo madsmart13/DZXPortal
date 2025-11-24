769816d61ce38bf159056145974555dc5e55fb0e
@Irgame_blz <mahdiyarmoradi93@gmail.com>
2025-11-24 04:30:47 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); 
+app.UseStaticFiles();
+
+app.Run();
